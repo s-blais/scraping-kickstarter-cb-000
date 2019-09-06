45998273ead50e2e4d2b25a76b6a29d65dfd267c
Stephen Blais <stephen@s-blais.com>
2019-09-06 20:31:27 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -5,6 +5,9 @@ def create_project_hash
   html = File.read('fixtures/kickstarter.html')
   kickstarter = Nokogiri::HTML(html)
   binding.pry
+
+  # projects: kickstarter.css(".project-card")
+  # title: project.css("h2.bbcard_name strong a").text
 end
 
 create_project_hash
